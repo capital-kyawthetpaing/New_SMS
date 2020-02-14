64972e13c7ab4ae07325cd859af39e2f3940f263
capital-phyoethurakyaw <60682417+capital-phyoethurakyaw@users.noreply.github.com>
2020-02-14 10:59:37 +0630

    Mg Ya Kouk

---
+++ Amazon_API/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon_API
+{
+    class Program
+    {
+      public static void Main(string[] args)
+        {
+
+
+        }
+
+    }
+}
