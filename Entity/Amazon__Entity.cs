451be26daa475b5730174bf83ad833735102bc30
capital-phyoethurakyaw <60682417+capital-phyoethurakyaw@users.noreply.github.com>
2020-03-26 15:50:52 +0630

    Phyoe Gyii 'S Amazon API

--- Entity/Amazon__Entity.cs
+++ Entity/Amazon__Entity.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 using System.Data;
 namespace Entity
 {
-    class Amazon__Entity
+   public class Amazon__Entity:Base_Entity
     {
+        public string strAccessKeyId { get; set; }
+        public  string strSecretKeyId { get; set; }
+        public  string strMerchantId { get; set; }
+        public  string strMarketplaceId { get; set; }
+        public  string strApplicationName { get; set; }
+        public  string strApplicationVersion { get; set; }
+        public  string strMWSAuthToken { get; set; }
+        public  string strServiceURL { get; set; }
+        public string StoreCD { get; set; }
+        public string APIKey { get; set; }
+        public string LastUpdatedAfter { get; set; }
+        public string LastUpdatedBefore { get; set; }
+        public string Xmldetails { get; set; }
+        public string XmlOrder { get; set; }
+
+        public string XmlOrderItems { get; set; }
+
     }
 }
