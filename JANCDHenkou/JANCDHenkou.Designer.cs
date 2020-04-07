9120242f6ed23cb17ab6b1b471eb1b8a8135f5ed
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-04-07 17:37:56 +0630

    By SawLay

--- JANCDHenkou/JANCDHenkou.Designer.cs
+++ JANCDHenkou/JANCDHenkou.Designer.cs
@@ -33,6 +33,10 @@
             this.panelDetail = new System.Windows.Forms.Panel();
             this.BtnF11Show = new CKM_Controls.CKM_Button();
             this.dgvJANCDHenkou = new CKM_Controls.CKM_GridView();
+            this.ckM_Label4 = new CKM_Controls.CKM_Label();
+            this.ckM_Label3 = new CKM_Controls.CKM_Label();
+            this.ckM_Label2 = new CKM_Controls.CKM_Label();
+            this.ckM_Label1 = new CKM_Controls.CKM_Label();
             this.colGenJanCD = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colBtnJAN = new System.Windows.Forms.DataGridViewButtonColumn();
             this.colBrandCD = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -43,10 +47,7 @@
             this.colColor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colGenJanCD2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colNewJANCD = new SMS.CustomControls.DataGridViewDecimalColumn();
-            this.ckM_Label4 = new CKM_Controls.CKM_Label();
-            this.ckM_Label3 = new CKM_Controls.CKM_Label();
-            this.ckM_Label2 = new CKM_Controls.CKM_Label();
-            this.ckM_Label1 = new CKM_Controls.CKM_Label();
+            this.colSKUCD = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panelDetail.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvJANCDHenkou)).BeginInit();
             this.SuspendLayout();
@@ -115,7 +116,8 @@
             this.colSize,
             this.colColor,
             this.colGenJanCD2,
-            this.colNewJANCD});
+            this.colNewJANCD,
+            this.colSKUCD});
             this.dgvJANCDHenkou.EnableHeadersVisualStyles = false;
             this.dgvJANCDHenkou.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(224)))), ((int)(((byte)(180)))));
             this.dgvJANCDHenkou.Location = new System.Drawing.Point(21, 145);
@@ -125,86 +127,7 @@
             this.dgvJANCDHenkou.UseRowNo = true;
             this.dgvJANCDHenkou.UseSetting = true;
             this.dgvJANCDHenkou.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvJANCDHenkou_CellContentClick);
-            // 
-            // colGenJanCD
-            // 
-            this.colGenJanCD.DataPropertyName = "GenJanCD";
-            this.colGenJanCD.HeaderText = "現JANCD";
-            this.colGenJanCD.MaxInputLength = 13;
-            this.colGenJanCD.Name = "colGenJanCD";
-            // 
-            // colBtnJAN
-            // 
-            this.colBtnJAN.HeaderText = "";
-            this.colBtnJAN.Name = "colBtnJAN";
-            this.colBtnJAN.Text = "";
-            this.colBtnJAN.Width = 30;
-            // 
-            // colBrandCD
-            // 
-            this.colBrandCD.DataPropertyName = "BrandCD";
-            this.colBrandCD.HeaderText = "";
-            this.colBrandCD.Name = "colBrandCD";
-            this.colBrandCD.ReadOnly = true;
-            this.colBrandCD.Width = 80;
-            // 
-            // colBrandName
-            // 
-            this.colBrandName.DataPropertyName = "BrandName";
-            this.colBrandName.HeaderText = "ブランド";
-            this.colBrandName.Name = "colBrandName";
-            this.colBrandName.ReadOnly = true;
-            this.colBrandName.Width = 150;
-            // 
-            // colITEM
-            // 
-            this.colITEM.DataPropertyName = "ITEM";
-            this.colITEM.HeaderText = "ITEM";
-            this.colITEM.Name = "colITEM";
-            this.colITEM.Width = 250;
-            // 
-            // colSKUName
-            // 
-            this.colSKUName.DataPropertyName = "SKUName";
-            this.colSKUName.HeaderText = "商品名";
-            this.colSKUName.Name = "colSKUName";
-            this.colSKUName.ReadOnly = true;
-            this.colSKUName.Width = 500;
-            // 
-            // colSize
-            // 
-            this.colSize.DataPropertyName = "Size";
-            this.colSize.HeaderText = "サイズ";
-            this.colSize.Name = "colSize";
-            this.colSize.ReadOnly = true;
-            this.colSize.Width = 150;
-            // 
-            // colColor
-            // 
-            this.colColor.DataPropertyName = "Color";
-            this.colColor.HeaderText = "カラー";
-            this.colColor.Name = "colColor";
-            this.colColor.ReadOnly = true;
-            this.colColor.Width = 150;
-            // 
-            // colGenJanCD2
-            // 
-            this.colGenJanCD2.DataPropertyName = "GenJanCD2";
-            this.colGenJanCD2.HeaderText = "現JANCD";
-            this.colGenJanCD2.Name = "colGenJanCD2";
-            this.colGenJanCD2.ReadOnly = true;
-            // 
-            // colNewJANCD
-            // 
-            this.colNewJANCD.DataPropertyName = "newJanCD";
-            this.colNewJANCD.DecimalPlace = ((byte)(0));
-            this.colNewJANCD.HeaderText = "新JANCD";
-            this.colNewJANCD.MaxInputLength = 13;
-            this.colNewJANCD.Name = "colNewJANCD";
-            this.colNewJANCD.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.colNewJANCD.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            this.colNewJANCD.UseMinus = false;
-            this.colNewJANCD.UseThousandSeparator = false;
+            this.dgvJANCDHenkou.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvJANCDHenkou_CellEndEdit);
             // 
             // ckM_Label4
             // 
@@ -274,6 +197,94 @@
             this.ckM_Label1.Text_Color = CKM_Controls.CKM_Label.CKM_Color.Default;
             this.ckM_Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
+            // colGenJanCD
+            // 
+            this.colGenJanCD.DataPropertyName = "GenJanCD";
+            this.colGenJanCD.HeaderText = "現JANCD";
+            this.colGenJanCD.MaxInputLength = 13;
+            this.colGenJanCD.Name = "colGenJanCD";
+            // 
+            // colBtnJAN
+            // 
+            this.colBtnJAN.HeaderText = "";
+            this.colBtnJAN.Name = "colBtnJAN";
+            this.colBtnJAN.Text = "";
+            this.colBtnJAN.Width = 30;
+            // 
+            // colBrandCD
+            // 
+            this.colBrandCD.DataPropertyName = "BrandCD";
+            this.colBrandCD.HeaderText = "";
+            this.colBrandCD.Name = "colBrandCD";
+            this.colBrandCD.ReadOnly = true;
+            this.colBrandCD.Width = 80;
+            // 
+            // colBrandName
+            // 
+            this.colBrandName.DataPropertyName = "BrandName";
+            this.colBrandName.HeaderText = "ブランド";
+            this.colBrandName.Name = "colBrandName";
+            this.colBrandName.ReadOnly = true;
+            this.colBrandName.Width = 150;
+            // 
+            // colITEM
+            // 
+            this.colITEM.DataPropertyName = "ITEM";
+            this.colITEM.HeaderText = "ITEM";
+            this.colITEM.Name = "colITEM";
+            this.colITEM.Width = 250;
+            // 
+            // colSKUName
+            // 
+            this.colSKUName.DataPropertyName = "SKUName";
+            this.colSKUName.HeaderText = "商品名";
+            this.colSKUName.Name = "colSKUName";
+            this.colSKUName.ReadOnly = true;
+            this.colSKUName.Width = 500;
+            // 
+            // colSize
+            // 
+            this.colSize.DataPropertyName = "SizeName";
+            this.colSize.HeaderText = "サイズ";
+            this.colSize.Name = "colSize";
+            this.colSize.ReadOnly = true;
+            this.colSize.Width = 150;
+            // 
+            // colColor
+            // 
+            this.colColor.DataPropertyName = "ColorName";
+            this.colColor.HeaderText = "カラー";
+            this.colColor.Name = "colColor";
+            this.colColor.ReadOnly = true;
+            this.colColor.Width = 150;
+            // 
+            // colGenJanCD2
+            // 
+            this.colGenJanCD2.DataPropertyName = "GenJanCD2";
+            this.colGenJanCD2.HeaderText = "現JANCD";
+            this.colGenJanCD2.Name = "colGenJanCD2";
+            this.colGenJanCD2.ReadOnly = true;
+            // 
+            // colNewJANCD
+            // 
+            this.colNewJANCD.DataPropertyName = "newJanCD";
+            this.colNewJANCD.DecimalPlace = ((byte)(0));
+            this.colNewJANCD.HeaderText = "新JANCD";
+            this.colNewJANCD.MaxInputLength = 13;
+            this.colNewJANCD.Name = "colNewJANCD";
+            this.colNewJANCD.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.colNewJANCD.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.colNewJANCD.UseMinus = false;
+            this.colNewJANCD.UseThousandSeparator = false;
+            // 
+            // colSKUCD
+            // 
+            this.colSKUCD.DataPropertyName = "SKUCD";
+            this.colSKUCD.HeaderText = "SKUCD";
+            this.colSKUCD.Name = "colSKUCD";
+            this.colSKUCD.ReadOnly = true;
+            this.colSKUCD.Visible = false;
+            // 
             // JANCDHenkou
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -315,6 +326,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colColor;
         private System.Windows.Forms.DataGridViewTextBoxColumn colGenJanCD2;
         private SMS.CustomControls.DataGridViewDecimalColumn colNewJANCD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSKUCD;
     }
 }
 
--- JANCDHenkou/JANCDHenkou.cs
+++ JANCDHenkou/JANCDHenkou.cs
@@ -18,7 +18,8 @@ namespace JANCDHenkou
     public partial class JANCDHenkou : FrmMainForm
     {
         JANCDHenkou_BL jhbl;
-        DataTable dtDisplay;
+        public bool dup, isExist = true;
+      
         public JANCDHenkou()
         {
             InitializeComponent();
@@ -39,7 +40,7 @@ namespace JANCDHenkou
             Btn_F10.Text = string.Empty;
             Btn_F11.Text = "取込(F11)";
  
-            dgvJANCDHenkou.DataSource = dtDisplay;
+            
         }
         private void JANCDHenkou_KeyUp(object sender, KeyEventArgs e)
         {
@@ -89,37 +90,33 @@ namespace JANCDHenkou
         private void dgvJANCDHenkou_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             var senderGrid = (DataGridView)sender;
-
-            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
-                e.RowIndex >= 0)
+            if (dgvJANCDHenkou.Rows[e.RowIndex].Cells["colGenJanCD"].Value  != null)
             {
-                Select_SKU frmSku = new Select_SKU();
-                frmSku.parJANCD = dgvJANCDHenkou.Rows[e.RowIndex].Cells["colGenJanCD"].Value.ToString();
-                frmSku.parChangeDate = System.DateTime.Now.ToString("yyyy-MM-dd");
-                frmSku.ShowDialog();
+                if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+                    e.RowIndex >= 0)
+                {
+                    Select_SKU frmSku = new Select_SKU();
+                    frmSku.parJANCD = dgvJANCDHenkou.Rows[e.RowIndex].Cells["colGenJanCD"].Value.ToString();
+                    frmSku.parChangeDate = System.DateTime.Now.ToString("yyyy-MM-dd");
+                    frmSku.ShowDialog();
+                }
             }
         }
 
         private bool ErrorCheck()
         {
-            dgvJANCDHenkou.DataSource = dtDisplay;
-
+           
             foreach (DataGridViewRow row in dgvJANCDHenkou.Rows)
             {
                 if (!(row.Cells["colGenJanCD"].Value == null))
                 {
-                    if (jhbl.SimpleSelect1("58", System.DateTime.Now.ToString("yyyy-MM-dd"),row.Cells["colGenJanCD"].Value.ToString()).Rows.Count < 1)
+                    if (jhbl.SimpleSelect1("59", System.DateTime.Now.ToString("yyyy-MM-dd"),row.Cells["colGenJanCD"].Value.ToString()).Rows.Count < 1)
                     {
                         jhbl.ShowMessage("E101");
                         return false;
                     }
                 }
-
-                if (dtDisplay.Rows.Count > 0)
-                {
-
-                }
-
+           
                 if (!(row.Cells["colNewJANCD"].Value == null))
                 {
                     if (jhbl.SimpleSelect1("59", System.DateTime.Now.ToString("yyyy-MM-dd"), row.Cells["colNewJanCD"].Value.ToString()).Rows.Count > 0)
@@ -149,6 +146,79 @@ namespace JANCDHenkou
         private void BtnF11Show_Click(object sender, EventArgs e)
         {
             F11();
+        }
+
+        private void dgvJANCDHenkou_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if ((sender as DataGridView).CurrentCell is DataGridViewTextBoxCell)
+            {
+                // 現JANCD
+                if ((dgvJANCDHenkou.CurrentCell == dgvJANCDHenkou.Rows[e.RowIndex].Cells["colGenJanCD"]) && !(dgvJANCDHenkou.Rows[e.RowIndex].Cells["colGenJanCD"].Value == null))
+                {
+
+                    foreach (DataGridViewRow r in dgvJANCDHenkou.Rows) //duplicate error
+                    {
+                        if (r.Index != e.RowIndex & !r.IsNewRow)
+                        {
+                            if (r.Cells["colGenJanCD"].Value.ToString() == dgvJANCDHenkou.CurrentRow.Cells["colGenJanCD"].Value.ToString())
+                            {
+                                jhbl.ShowMessage("E226");
+                                return;
+                            }
+                        }
+                    }
+
+                    DataTable dtGenJanCD = jhbl.SimpleSelect1("59", System.DateTime.Now.ToString("yyyy-MM-dd"), dgvJANCDHenkou.Rows[e.RowIndex].Cells["colGenJanCD"].Value.ToString());
+                    if (dtGenJanCD.Rows.Count < 1) // If its not exists then error
+                    {
+                        jhbl.ShowMessage("E101");
+                    }
+                    else if (dtGenJanCD.Rows.Count == 1)
+                    {
+                        //dgvJANCDHenkou.DataSource = dtGenJanCD;
+                       
+                        
+                    }
+                    else
+                    {
+
+                    }
+
+                    
+                }
+                // 現JANCD
+
+                // 新JANCD
+                if ((dgvJANCDHenkou.CurrentCell == dgvJANCDHenkou.Rows[e.RowIndex].Cells["colNewJanCD"]) && !(dgvJANCDHenkou.Rows[e.RowIndex].Cells["colNewJanCD"].Value == null))
+                {
+                    DataTable dtGenJanCD = jhbl.SimpleSelect1("59", System.DateTime.Now.ToString("yyyy-MM-dd"), dgvJANCDHenkou.Rows[e.RowIndex].Cells["colNewJanCD"].Value.ToString());
+                    if (dtGenJanCD.Rows.Count > 0)
+                    {
+                        isExist = false;
+                    }
+
+                    foreach (DataGridViewRow r in dgvJANCDHenkou.Rows) //duplicate error
+                    {
+                        if (r.Index != e.RowIndex & !r.IsNewRow)
+                        {
+                            if (r.Cells["colNewJanCD"].Value.ToString() == dgvJANCDHenkou.CurrentRow.Cells["colNewJanCD"].Value.ToString())
+                            {
+                                dup = false;
+                            }
+                        }
+                    }
+
+                    if (!isExist || !dup)
+                    {
+                        DialogResult dr = jhbl.ShowMessage("Q316");
+                        if (dr == DialogResult.No)
+                        {
+                            dgvJANCDHenkou.CurrentCell = dgvJANCDHenkou.CurrentRow.Cells["colNewJanCD"];
+                        }
+                    }
+                }
+                // 新JANCD
+            }
         }
     }
 }
