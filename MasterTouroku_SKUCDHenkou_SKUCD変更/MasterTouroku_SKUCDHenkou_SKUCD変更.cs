65c4549df6ed9da45b0ca20855ff18608ba2eae9
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2020-04-08 12:46:50 +0630

    pnz

---
+++ MasterTouroku_SKUCDHenkou_SKUCD変更/MasterTouroku_SKUCDHenkou_SKUCD変更.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Base.Client;
+
+namespace MasterTouroku_SKUCDHenkou_SKUCD変更
+{
+    public partial class MasterTouroku_SKUCDHenkou_SKUCD変更 : FrmMainForm
+    {
+        public MasterTouroku_SKUCDHenkou_SKUCD変更()
+        {
+            InitializeComponent();
+        }
+    }
+}
