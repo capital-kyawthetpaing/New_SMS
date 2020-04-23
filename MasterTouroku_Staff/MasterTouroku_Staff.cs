25ff83e75403e2edb076a54d63332db737d796e3
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-04-23 12:37:00 +0630

    BY SawLay

--- MasterTouroku_Staff/MasterTouroku_Staff.cs
+++ MasterTouroku_Staff/MasterTouroku_Staff.cs
@@ -450,7 +450,7 @@ namespace MasterTouroku_Staff
                     EnablePanel(PanelHeader);
                     DisablePanel(PanelDetail);
                     ScStaff.SearchEnable = false;
-                    ScStaffCopyCD.SearchEnable = true;
+                    ScStaffCopyCD.SearchEnable = true; 
                     F9Visible = false;
                     F12Enable = true;
                     btnDisplay.Enabled = F11Enable = true;
