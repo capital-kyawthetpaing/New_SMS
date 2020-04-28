d8a5667fb07c931d4198074dd82a1eb92adc9e07
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-04-28 11:38:51 +0630

    By SawLay

--- MasterTouroku_YuubinBangou/MasterTouroku_YuubinBangou.cs
+++ MasterTouroku_YuubinBangou/MasterTouroku_YuubinBangou.cs
@@ -301,13 +301,13 @@ namespace MasterTouroku_YuubinBangou
                                 return false;
                             }
 
-                            if (!string.IsNullOrWhiteSpace(row.Cells["colZipCD1"].Value.ToString()) && string.IsNullOrWhiteSpace(row.Cells["colAdd2"].Value.ToString()))
-                            {
-                                YuubinBangouBL.ShowMessage("E102");
-                                dgvYuubinBangou.CurrentCell = row.Cells["colAdd2"];
-                                dgvYuubinBangou.BeginEdit(true);
-                                return false;
-                            }
+                            //if (!string.IsNullOrWhiteSpace(row.Cells["colZipCD1"].Value.ToString()) && string.IsNullOrWhiteSpace(row.Cells["colAdd2"].Value.ToString()))
+                            //{
+                            //    YuubinBangouBL.ShowMessage("E102");
+                            //    dgvYuubinBangou.CurrentCell = row.Cells["colAdd2"];
+                            //    dgvYuubinBangou.BeginEdit(true);
+                            //    return false;
+                            //}
                         }
                         //else if(!string.IsNullOrWhiteSpace(row.Cells["colZipCD2"].Value.ToString()) && (string.IsNullOrWhiteSpace(row.Cells["colZipCD1"].Value.ToString())))
                         //{
@@ -508,11 +508,11 @@ namespace MasterTouroku_YuubinBangou
                 }
                 else if (dgvYuubinBangou.CurrentCell == dgvYuubinBangou.Rows[e.RowIndex].Cells["colAdd2"])
                 {
-                    if (!string.IsNullOrWhiteSpace(dgvYuubinBangou.Rows[e.RowIndex].Cells["colZipCD1"].Value.ToString()) && string.IsNullOrWhiteSpace(dgvYuubinBangou.Rows[e.RowIndex].Cells["colAdd2"].Value.ToString()))
-                    {
-                        YuubinBangouBL.ShowMessage("E102");
-                        dgvYuubinBangou.CurrentCell = dgvYuubinBangou.Rows[e.RowIndex].Cells["colAdd2"];
-                    }
+                    //if (!string.IsNullOrWhiteSpace(dgvYuubinBangou.Rows[e.RowIndex].Cells["colZipCD1"].Value.ToString()) && string.IsNullOrWhiteSpace(dgvYuubinBangou.Rows[e.RowIndex].Cells["colAdd2"].Value.ToString()))
+                    //{
+                    //    YuubinBangouBL.ShowMessage("E102");
+                    //    dgvYuubinBangou.CurrentCell = dgvYuubinBangou.Rows[e.RowIndex].Cells["colAdd2"];
+                    //}
                     if (string.IsNullOrWhiteSpace(dgvYuubinBangou.Rows[e.RowIndex].Cells["colZipCD1"].Value.ToString()) && !string.IsNullOrWhiteSpace(dgvYuubinBangou.Rows[e.RowIndex].Cells["colAdd2"].Value.ToString()))
                     {
                         dgvYuubinBangou.Rows[e.RowIndex].Cells["colAdd2"].Value = null;
