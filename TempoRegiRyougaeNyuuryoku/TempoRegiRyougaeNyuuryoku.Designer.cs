11e98214821c2771e566e55ffa721be28f3b6547
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2020-05-11 14:12:04 +0630

    by SES

--- TempoRegiRyougaeNyuuryoku/TempoRegiRyougaeNyuuryoku.Designer.cs
+++ TempoRegiRyougaeNyuuryoku/TempoRegiRyougaeNyuuryoku.Designer.cs
@@ -207,7 +207,7 @@
             this.ckmShop_Label6.Font = new System.Drawing.Font("MS Gothic", 26F, System.Drawing.FontStyle.Bold);
             this.ckmShop_Label6.Font_Size = CKM_Controls.CKMShop_Label.CKM_FontSize.Normal;
             this.ckmShop_Label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(130)))), ((int)(((byte)(53)))));
-            this.ckmShop_Label6.Location = new System.Drawing.Point(85, 198);
+            this.ckmShop_Label6.Location = new System.Drawing.Point(85, 200);
             this.ckmShop_Label6.Name = "ckmShop_Label6";
             this.ckmShop_Label6.Size = new System.Drawing.Size(163, 35);
             this.ckmShop_Label6.TabIndex = 13;
@@ -242,14 +242,14 @@
             this.ExchangeDenomination.ComboAlign = CKM_Controls.CKMShop_ComboBox.Align.right;
             this.ExchangeDenomination.Ctrl_Byte = CKM_Controls.CKMShop_ComboBox.Bytes.半全角;
             this.ExchangeDenomination.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.ExchangeDenomination.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.ExchangeDenomination.Font = new System.Drawing.Font("MS Gothic", 18F);
             this.ExchangeDenomination.FontSize_ = 18F;
             this.ExchangeDenomination.FormattingEnabled = true;
             this.ExchangeDenomination.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
             this.ExchangeDenomination.ItemHeight = 26;
             this.ExchangeDenomination.ItemHeight_ = 26;
             this.ExchangeDenomination.Length = 20;
-            this.ExchangeDenomination.Location = new System.Drawing.Point(251, 203);
+            this.ExchangeDenomination.Location = new System.Drawing.Point(251, 205);
             this.ExchangeDenomination.MaxItem = 8;
             this.ExchangeDenomination.MaxLength = 20;
             this.ExchangeDenomination.MoveNext = true;
