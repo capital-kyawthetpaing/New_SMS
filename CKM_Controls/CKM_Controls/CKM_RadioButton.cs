fb0b507bec5c9b2a97b341a1911c76547583a8f4
kyaw thet paing <kyawthetpaing.mm@gmail.com>
2020-05-13 18:08:45 +0630

    ktp

--- CKM_Controls/CKM_Controls/CKM_RadioButton.cs
+++ CKM_Controls/CKM_Controls/CKM_RadioButton.cs
@@ -9,10 +9,23 @@ namespace CKM_Controls
 {
     public class CKM_RadioButton : RadioButton
     {
+        protected override bool ShowFocusCues
+        {
+            get { return true; }
+        }
+        protected override void OnGotFocus(EventArgs e)
+        {
+            base.OnGotFocus(e);
+        }
+        protected override void OnLostFocus(EventArgs e)
+        {
+            base.OnLostFocus(e);
+        }
         public CKM_RadioButton()
         {
             this.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Bold);
         }
-     
+
+        
     }
 }
