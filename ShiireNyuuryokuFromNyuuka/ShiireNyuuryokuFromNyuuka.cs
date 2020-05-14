af5cac62f3e0f8ab09d645afef70cd0654cf0627
Fumi Sato <hesaka@dus.co.jp>
2020-05-14 17:04:33 +0900

    hesaka NO394

--- ShiireNyuuryokuFromNyuuka/ShiireNyuuryokuFromNyuuka.cs
+++ ShiireNyuuryokuFromNyuuka/ShiireNyuuryokuFromNyuuka.cs
@@ -1902,7 +1902,8 @@ namespace ShiireNyuuryokuFromNyuuka
             if (!chkAll && !changeYmd)
             {
                 int w_CtlRow = row - Vsb_Mei_0.Value;
-                if (mGrid.g_MK_Ctrl[col, w_CtlRow].CellCtl.GetType().Equals(typeof(CKM_Controls.CKM_TextBox)))
+                if (w_CtlRow < ClsGridShiire.gc_P_GYO)
+                    if (mGrid.g_MK_Ctrl[col, w_CtlRow].CellCtl.GetType().Equals(typeof(CKM_Controls.CKM_TextBox)))
                 {
                     if (((CKM_Controls.CKM_TextBox)mGrid.g_MK_Ctrl[col, w_CtlRow].CellCtl).isMaxLengthErr)
                         return false;
--- TempoJuchuuNyuuryoku/TempoJuchuuNyuuryoku.cs
+++ TempoJuchuuNyuuryoku/TempoJuchuuNyuuryoku.cs
@@ -2756,7 +2756,8 @@ namespace TempoJuchuuNyuuryoku
             if (!chkAll && !changeYmd)
             {
                 int w_CtlRow = row - Vsb_Mei_0.Value;
-                if (mGrid.g_MK_Ctrl[col, w_CtlRow].CellCtl.GetType().Equals(typeof(CKM_Controls.CKM_TextBox)))
+                if (w_CtlRow < ClsGridJuchuu.gc_P_GYO)
+                    if (mGrid.g_MK_Ctrl[col, w_CtlRow].CellCtl.GetType().Equals(typeof(CKM_Controls.CKM_TextBox)))
                 {
                     if (((CKM_Controls.CKM_TextBox)mGrid.g_MK_Ctrl[col, w_CtlRow].CellCtl).isMaxLengthErr)
                         return false;
