79965c70c698289beb3c987a29f55efbebda9f04
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2020-05-21 08:42:31 +0630

    by SES

--- TempoRegiRyougaeNyuuryoku/TempoRegiRyougaeNyuuryoku.Designer.cs
+++ TempoRegiRyougaeNyuuryoku/TempoRegiRyougaeNyuuryoku.Designer.cs
@@ -111,7 +111,7 @@
             this.ckmShop_Label4.Font = new System.Drawing.Font("MS Gothic", 26F, System.Drawing.FontStyle.Bold);
             this.ckmShop_Label4.Font_Size = CKM_Controls.CKMShop_Label.CKM_FontSize.Normal;
             this.ckmShop_Label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(130)))), ((int)(((byte)(53)))));
-            this.ckmShop_Label4.Location = new System.Drawing.Point(123, 284);
+            this.ckmShop_Label4.Location = new System.Drawing.Point(123, 280);
             this.ckmShop_Label4.Name = "ckmShop_Label4";
             this.ckmShop_Label4.Size = new System.Drawing.Size(125, 35);
             this.ckmShop_Label4.TabIndex = 5;
@@ -127,7 +127,7 @@
             this.ckmShop_Label5.Font = new System.Drawing.Font("MS Gothic", 26F, System.Drawing.FontStyle.Bold);
             this.ckmShop_Label5.Font_Size = CKM_Controls.CKMShop_Label.CKM_FontSize.Normal;
             this.ckmShop_Label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(130)))), ((int)(((byte)(53)))));
-            this.ckmShop_Label5.Location = new System.Drawing.Point(123, 355);
+            this.ckmShop_Label5.Location = new System.Drawing.Point(123, 352);
             this.ckmShop_Label5.Name = "ckmShop_Label5";
             this.ckmShop_Label5.Size = new System.Drawing.Size(125, 35);
             this.ckmShop_Label5.TabIndex = 6;
@@ -153,7 +153,7 @@
             this.ExchangeCount.IsNumber = true;
             this.ExchangeCount.IsShop = false;
             this.ExchangeCount.Length = 10;
-            this.ExchangeCount.Location = new System.Drawing.Point(251, 281);
+            this.ExchangeCount.Location = new System.Drawing.Point(251, 277);
             this.ExchangeCount.MaxLength = 10;
             this.ExchangeCount.MoveNext = true;
             this.ExchangeCount.Name = "ExchangeCount";
@@ -171,7 +171,7 @@
             this.Remark.Font = new System.Drawing.Font("MS Gothic", 26F);
             this.Remark.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
             this.Remark.Length = 200;
-            this.Remark.Location = new System.Drawing.Point(251, 352);
+            this.Remark.Location = new System.Drawing.Point(251, 349);
             this.Remark.MaxLength = 200;
             this.Remark.Mdea = false;
             this.Remark.Mfocus = false;
@@ -266,7 +266,7 @@
             this.ExchangeLabel.Font = new System.Drawing.Font("MS Gothic", 28F, System.Drawing.FontStyle.Bold);
             this.ExchangeLabel.Font_Size = CKM_Controls.CKMShop_Label.CKM_FontSize.Medium;
             this.ExchangeLabel.ForeColor = System.Drawing.Color.Black;
-            this.ExchangeLabel.Location = new System.Drawing.Point(444, 281);
+            this.ExchangeLabel.Location = new System.Drawing.Point(444, 277);
             this.ExchangeLabel.Name = "ExchangeLabel";
             this.ExchangeLabel.Size = new System.Drawing.Size(168, 42);
             this.ExchangeLabel.TabIndex = 14;
