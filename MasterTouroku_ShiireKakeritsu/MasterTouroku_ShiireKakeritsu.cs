91c99c44098b512fb2a5390c11a305b3a44e1b6a
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-05-22 08:43:45 +0630

    By SawLay

--- MasterTouroku_ShiireKakeritsu/MasterTouroku_ShiireKakeritsu.cs
+++ MasterTouroku_ShiireKakeritsu/MasterTouroku_ShiireKakeritsu.cs
@@ -916,26 +916,22 @@ namespace MasterTouroku_ShiireKakeritsu
         protected Boolean CheckColumn(String[] colName,DataTable dtMain) //Check Columns if require columns are exist in import excel
         {
             DataColumnCollection col = dtMain.Columns;
-            for (int i = 0; i < colName.Length; i++)
+            //for (int i = 0; i < colName.Length; i++)
             {
-                if (!dtMain.Columns[1].ColumnName.ToString().Equals("仕入先CD"))
+                if (!dtMain.Columns[0].ColumnName.ToString().Equals("仕入先CD"))
                 {
-                    mskbl.ShowMessage("E137");
                     return false;
                 }
-                else if (!dtMain.Columns[2].ColumnName.ToString().Equals("店舗CD"))
+                else if (!dtMain.Columns[1].ColumnName.ToString().Equals("店舗CD"))
                 {
-                    mskbl.ShowMessage("E137");
                     return false;
                 }
-                else if (!dtMain.Columns[8].ColumnName.ToString().Equals("改定日"))
+                else if (!dtMain.Columns[7].ColumnName.ToString().Equals("改定日"))
                 {
-                    mskbl.ShowMessage("E137");
                     return false;
                 }
-                else if (!dtMain.Columns[9].ColumnName.ToString().Equals("掛率"))
+                else if (!dtMain.Columns[8].ColumnName.ToString().Equals("掛率"))
                 {
-                    mskbl.ShowMessage("E137");
                     return false;
                 }
             }
@@ -979,5 +975,5 @@ namespace MasterTouroku_ShiireKakeritsu
                 F10();
             }
         }
-    }
+    //}
 }
