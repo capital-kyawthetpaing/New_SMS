695c9aded9c12b829a10e422835974bf5facfea1
capital-poeeisan <capital.poeeisan@gmail.com>
2020-05-29 17:36:36 +0630

    pes

--- DL/MasterTouroku_ShiireTanka_DL.cs
+++ DL/MasterTouroku_ShiireTanka_DL.cs
@@ -19,12 +19,24 @@ namespace DL
                 { "@storecd", new ValuePair { value1 = SqlDbType.VarChar, value2 = mio.StoreCD } },
                 { "@changedate", new ValuePair { value1 = SqlDbType.VarChar, value2 = mio.ChangeDate } },
                 { "@makerItem", new ValuePair { value1 = SqlDbType.VarChar, value2 = mio.MakerItem } },
+                { "@display", new ValuePair { value1 = SqlDbType.VarChar, value2 = mio.Display } },
                 { "@brandcd", new ValuePair { value1 = SqlDbType.VarChar, value2 = mi.BrandCD } },
                 { "@sportcd", new ValuePair { value1 = SqlDbType.VarChar, value2 = mi.SportsCD} },
                 { "@segmentcd", new ValuePair { value1 = SqlDbType.VarChar, value2 = mi.SegmentCD } },
                 { "@lastyearterm", new ValuePair { value1 = SqlDbType.VarChar, value2 = mi.LastYearTerm } },
                 { "@lastseason", new ValuePair { value1 = SqlDbType.VarChar, value2 = mi.LastSeason } },
-                { "@heardate", new ValuePair { value1 = SqlDbType.VarChar, value2 = mi.ChangeDate } },
+                { "@heardate", new ValuePair { value1 = SqlDbType.VarChar, value2 = mio.Headerdate } },
+            };
+            return SelectData(dic, sp);
+        }
+        public DataTable M_ITem_ItemNandPriceoutTax_Select(M_ITEM_Entity mi)
+        {
+            string sp = "M_ITem_ItemNandPriceoutTax_Select";
+            Dictionary<string, ValuePair> dic = new Dictionary<string, ValuePair>
+            {
+                { "@ITemCD", new ValuePair { value1 = SqlDbType.VarChar, value2 = mi.ITemCD } },
+                { "@AddDate", new ValuePair { value1 = SqlDbType.Date, value2 = mi.ChangeDate } },
+                
             };
             return SelectData(dic, sp);
         }
