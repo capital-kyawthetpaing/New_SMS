035b051fb37aba7647e27efdc3fdeae16a066ed2
Fumi Sato <hesaka@dus.co.jp>
2020-06-15 16:41:11 +0900

    hesaka

--- Entity/D_Pay_Entity.cs
+++ Entity/D_Pay_Entity.cs
@@ -11,6 +11,45 @@ namespace Entity
         public string PayNo { get; set; }
         public string LargePayNO { get; set; }
         public string PayeeCD { get; set; }
+        public string InputDateTime { get; set; }
+        //public string StaffCD { get; set; }
+        //public string PayDate { get; set; }
+        //public string PayPlanDate { get; set; }
+        public string HontaiGaku8 { get; set; }
+        public string HontaiGaku10 { get; set; }
+        public string TaxGaku8 { get; set; }
+        public string TaxGaku10 { get; set; }
+        public string PayGaku { get; set; }
+        public string NotPaidGaku { get; set; }
+        public string TransferGaku { get; set; }
+        public string TransferFeeGaku { get; set; }
+        public string FeeKBN { get; set; }
+        public string MotoKouzaCD { get; set; }
+        public string BankCD { get; set; }
+        public string BranchCD { get; set; }
+        public string KouzaKBN { get; set; }
+        public string KouzaNO { get; set; }
+        public string KouzaMeigi { get; set; }
+        public string CashGaku { get; set; }
+        public string BillGaku { get; set; }
+        public string BillDate { get; set; }
+        public string BillNO { get; set; }
+        public string ERMCGaku { get; set; }
+        public string ERMCDate { get; set; }
+        public string ERMCNO { get; set; }
+        public string CardGaku { get; set; }
+        public string OffsetGaku { get; set; }
+        public string OtherGaku1 { get; set; }
+        public string Account1 { get; set; }
+        public string SubAccount1 { get; set; }
+        public string OtherGaku2 { get; set; }
+        public string Account2 { get; set; }
+        public string SubAccount2 { get; set; }
+        public string FBCreateDate { get; set; }
+        public string FBCreateNO { get; set; }
+        //支払登録用Entitiy
+        
+        public string PayeeName { get; set; }
 
         //支払い一覧表フォームのため、Entityデータ要件
         public string PurchaseDateFrom { get; set; }
