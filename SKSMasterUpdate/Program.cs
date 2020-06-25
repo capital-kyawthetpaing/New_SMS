d70eef61ebb74340728707ee54ffe36e7bdf6b52
capital-eithinzarzaw <60596990+capital-eithinzarzaw@users.noreply.github.com>
2020-06-25 17:35:31 +0630

    etz

---
+++ SKSMasterUpdate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKSMasterUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "SKSMasterUpdate_SKS連携商基本商品";
+
+
+        }
+    }
+}
+ 
