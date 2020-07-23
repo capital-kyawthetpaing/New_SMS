2025aaaaeca4a282fedb76c10a54a7a6dfc4f78c
watanabe <watanabe@dus.co.jp>
2020-07-23 18:37:33 +0900

    watanabe 変更

--- DL/D_MarkDown_DL.cs
+++ DL/D_MarkDown_DL.cs
@@ -66,6 +66,8 @@ namespace DL
             command.CommandTimeout = 0;
 
             AddParam(command, "@OperateMode", SqlDbType.Int, operationMode.ToString());
+            AddParam(command, "@ChkResult", SqlDbType.VarChar, dme.ChkResult);
+            AddParam(command, "@ChangeDate", SqlDbType.VarChar, dme.ChangeDate);
             AddParam(command, "@MarkDownNO", SqlDbType.VarChar, dme.MarkDownNO);
             AddParam(command, "@StoreCD", SqlDbType.VarChar, dme.StoreCD);
             AddParam(command, "@SoukoCD", SqlDbType.VarChar, dme.SoukoCD);
@@ -81,7 +83,12 @@ namespace DL
             AddParam(command, "@Comment", SqlDbType.VarChar, dme.Comment);
             AddParam(command, "@MDPurchaseNO", SqlDbType.VarChar, dme.MDPurchaseNO);
             AddParam(command, "@PurchaseNO", SqlDbType.VarChar, dme.PurchaseNO);
-            AddParam(command, "@PurchaseGaku", SqlDbType.VarChar, dme.PurchaseGaku);
+            AddParam(command, "@PurchaseGaku", SqlDbType.Money, dme.PurchaseGaku);
+            AddParam(command, "@PurchaseTax", SqlDbType.Money, dme.PurchaseTax);
+            AddParam(command, "@TaxGaku8", SqlDbType.Money, dme.TaxGaku8);
+            AddParam(command, "@TaxGaku10", SqlDbType.Money, dme.TaxGaku10);
+            AddParam(command, "@HontaiGaku8", SqlDbType.Money, dme.HontaiGaku8);
+            AddParam(command, "@HontaiGaku10", SqlDbType.Money, dme.HontaiGaku10);
 
             AddParamForDataTable(command, "@Table", SqlDbType.Structured, dt);
             AddParam(command, "@Operator", SqlDbType.VarChar, dme.InsertOperator);
