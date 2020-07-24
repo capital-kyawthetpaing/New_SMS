8200157738851a3c51a583d70eb232dcc17b15d1
tanohata-tsuyoshi <tsuyoshi@tanohata3.jp>
2020-07-24 17:03:54 +0900

    店舗レジ取引ジャーナルの課題表修正 No.1221

--- DL/TempoRegiTorihikiReceipt_DL.cs
+++ DL/TempoRegiTorihikiReceipt_DL.cs
@@ -12,13 +12,15 @@ namespace DL
         /// 店舗取引レシート(雑入金)情報取得処理
         /// </summary>
         /// <param name="depositNo">入出金No</param>
+        /// <param name="staffCD">担当者CD</param>
         /// <returns>店舗取引レシート(雑入金)情報取得</returns>
-        public DataTable D_MiscDepositSelectData(string depositNo)
+        public DataTable D_MiscDepositSelectData(string depositNo, string staffCD)
         {
             return SelectData(
                 new Dictionary<string, ValuePair>
                 {
                     { "@depositNo", new ValuePair { value1 = SqlDbType.Int, value2 = depositNo } },
+                    { "@staffCD", new ValuePair { value1 = SqlDbType.VarChar, value2 = staffCD } },
                 },
                 "D_SelectMiscDeposit_ForTempoTorihikiReceipt"
             );
@@ -28,13 +30,15 @@ namespace DL
         /// 店舗取引レシート(入金)情報取得処理
         /// </summary>
         /// <param name="depositNo">入出金No</param>
+        /// <param name="staffCD">担当者CD</param>
         /// <returns>店舗取引レシート(入金)情報取得</returns>
-        public DataTable D_DepositSelectData(string depositNo)
+        public DataTable D_DepositSelectData(string depositNo, string staffCD)
         {
             return SelectData(
                 new Dictionary<string, ValuePair>
                 {
                     { "@depositNo", new ValuePair { value1 = SqlDbType.Int, value2 = depositNo } },
+                    { "@staffCD", new ValuePair { value1 = SqlDbType.VarChar, value2 = staffCD } },
                 },
                 "D_SelectDeposit_ForTempoTorihikiReceipt"
             );
@@ -44,13 +48,15 @@ namespace DL
         /// 店舗取引レシート(雑出金)情報取得処理
         /// </summary>
         /// <param name="depositNo">入出金No</param>
+        /// <param name="staffCD">担当者CD</param>
         /// <returns>店舗取引レシート(雑出金)情報取得</returns>
-        public DataTable D_MiscPaymentSelectData(string depositNo)
+        public DataTable D_MiscPaymentSelectData(string depositNo, string staffCD)
         {
             return SelectData(
                 new Dictionary<string, ValuePair>
                 {
                     { "@depositNo", new ValuePair { value1 = SqlDbType.Int, value2 = depositNo } },
+                    { "@staffCD", new ValuePair { value1 = SqlDbType.VarChar, value2 = staffCD } },
                 },
                 "D_SelectMiscPayment_ForTempoTorihikiReceipt"
             );
@@ -60,13 +66,15 @@ namespace DL
         /// 店舗取引レシート(両替)情報取得処理
         /// </summary>
         /// <param name="depositNo">入出金No</param>
+        /// <param name="staffCD">担当者CD</param>
         /// <returns>店舗取引レシート(両替)情報取得</returns>
-        public DataTable D_ExchangeSelectData(string depositNo)
+        public DataTable D_ExchangeSelectData(string depositNo, string staffCD)
         {
             return SelectData(
                 new Dictionary<string, ValuePair>
                 {
                     { "@depositNo", new ValuePair { value1 = SqlDbType.Int, value2 = depositNo } },
+                    { "@staffCD", new ValuePair { value1 = SqlDbType.VarChar, value2 = staffCD } },
                 },
                 "D_SelectExchange_ForTempoTorihikiReceipt"
             );
@@ -76,13 +84,15 @@ namespace DL
         /// 店舗取引レシート(釣銭準備)情報取得処理
         /// </summary>
         /// <param name="depositNo">入出金No</param>
+        /// <param name="staffCD">担当者CD</param>
         /// <returns>店舗取引レシート(釣銭準備)情報取得</returns>
-        public DataTable D_ChangePreparationSelectData(string depositNo)
+        public DataTable D_ChangePreparationSelectData(string depositNo, string staffCD)
         {
             return SelectData(
                 new Dictionary<string, ValuePair>
                 {
                     { "@depositNo", new ValuePair { value1 = SqlDbType.Int, value2 = depositNo } },
+                    { "@staffCD", new ValuePair { value1 = SqlDbType.VarChar, value2 = staffCD } },
                 },
                 "D_SelectChangePreparation_ForTempoTorihikiReceipt"
             );
