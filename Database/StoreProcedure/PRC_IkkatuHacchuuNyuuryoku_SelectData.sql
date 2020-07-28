c762abb6acddbbf812e8cc18751db7a804ab25c6
capital-eithinzarzaw <60596990+capital-eithinzarzaw@users.noreply.github.com>
2020-07-28 13:42:22 +0630

    etz

--- Database/StoreProcedure/PRC_IkkatuHacchuuNyuuryoku_SelectData.sql
+++ Database/StoreProcedure/PRC_IkkatuHacchuuNyuuryoku_SelectData.sql
@@ -1,5 +1,5 @@
 --  ======================================================================  
---       Program Call    àÍäáî≠íçì¸óÕ  
+--       Program Call    ‰∏ÄÊã¨Áô∫Ê≥®ÂÖ•Âäõ  
 --       Program ID      IkkatuHacchuuNyuuryoku
 --       Create date:    2019.10.09
 --    ======================================================================  
@@ -11,7 +11,7 @@ end
 GO
   
 CREATE PROCEDURE PRC_IkkatuHacchuuNyuuryoku_SelectData(  
- @p_IkkatuHacchuuMode        varchar(1) --0:Netî≠íçÅA1:FAXî≠íç  
+ @p_IkkatuHacchuuMode        varchar(1) --0:NetÁô∫Ê≥®„ÄÅ1:FAXÁô∫Ê≥®  
 ,@p_HacchuuDate              varchar(10)  
 ,@p_VendorCD                 varchar(13)   
 ,@p_JuchuuStaffCD            varchar(13)   
@@ -30,8 +30,8 @@ BEGIN
           ,CAST(NULL AS varchar) AS HacchuuNO    
           ,DJUD.VendorCD   AS SiiresakiCD    
           ,MVEN.VendorName AS SiiresakiName    
-          ,CASE WHEN MSKU.DirectFlg = 1 THEN 'Åõ' ELSE NULL END AS ChokusouFLG    
-          ,CASE WHEN MSKU.NoNetOrderFlg = 0 THEN 'Åõ' ELSE 'Å~' END AS NetFLG    
+          ,CASE WHEN MSKU.DirectFlg = 1 THEN '‚óã' ELSE NULL END AS ChokusouFLG    
+          ,CASE WHEN MSKU.NoNetOrderFlg = 0 THEN '‚óã' ELSE '√ó' END AS NetFLG    
           ,CASE WHEN MSKU.DirectFlg = 0 THEN MSOU.SoukoName ELSE DDEP.DeliveryName END AS NounyuusakiName    
           ,MSKU.DirectFlg AS NounyuusakiKBN    
           ,CASE WHEN MSKU.DirectFlg = 0 THEN MSOU.Address1 + ' ' + MSOU.Address2 ELSE DDEP.DeliveryAddress1 + ' ' + DDEP.DeliveryAddress2 END AS NounyuusakiJuusho    
