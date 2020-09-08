45c2e168cbfaca648417498fa79723af5815b04e
capital-phyoethurakyaw <capital.phyoethurakyaw@gmail.com>
2020-09-08 11:09:51 +0630

    PTK

---
+++ Entity/Tenjikai_Entity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+  public  class Tenjikai_Entity :Base_Entity
+    {
+
+        public string TenjiKaiOrderNo { get; set; }
+        public string Shiiresaki { get; set; }
+        public string Nendo { get; set; }
+        public string ShuuKaSouKo { get; set; }
+        public string ShiZun { get; set; }
+        public string JuchuuBi { get; set; }
+        public string UriageYoteiBi { get; set; }
+        public string TantouStaffu { get; set; }
+
+        public string Kokyaku { get; set;}
+        public string C_Sama { get; set; }
+        public string C_OnChuu { get; set; }
+        public string C_DenwaBangou { get; set; }
+
+        public string HaisoSaki { get; set; }
+        public string S_Sama { get; set; }
+        public string S_OnChuu { get; set; }
+        public string S_DenwaBangou { get; set; }
+
+        public string YoteiKinShu { get; set; }
+
+
+    }
+}
