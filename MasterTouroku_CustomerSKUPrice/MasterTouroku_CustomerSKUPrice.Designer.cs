c6d97c44d54b9fc0ec9b26b36689fa91101c8fd4
capital-eithinzarzaw <60596990+capital-eithinzarzaw@users.noreply.github.com>
2020-09-15 10:18:58 +0630

    etz

---
+++ MasterTouroku_CustomerSKUPrice/MasterTouroku_CustomerSKUPrice.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MasterTouroku_CustomerSKUPrice
+{
+    partial class FrmMasterTouroku_CustomerSKUPrice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // PanelHeader
+            // 
+            this.PanelHeader.Location = new System.Drawing.Point(0, 0);
+            this.PanelHeader.Size = new System.Drawing.Size(1420, 48);
+            // 
+            // PanelSearch
+            // 
+            this.PanelSearch.Location = new System.Drawing.Point(886, 0);
+            // 
+            // btnChangeIkkatuHacchuuMode
+            // 
+            this.btnChangeIkkatuHacchuuMode.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(2, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1410, 630);
+            this.panel1.TabIndex = 100;
+            // 
+            // FrmMasterTouroku_CustomerSKUPrice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1422, 718);
+            this.Controls.Add(this.panel1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.ModeVisible = true;
+            this.Name = "FrmMasterTouroku_CustomerSKUPrice";
+            this.PanelHeaderHeight = 50;
+            this.Text = "顧客別SKU販売単価マスタ";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ MasterTouroku_CustomerSKUPrice/MasterTouroku_CustomerSKUPrice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Base.Client;
+
+namespace MasterTouroku_CustomerSKUPrice
+{
+    public partial class FrmMasterTouroku_CustomerSKUPrice : FrmMainForm
+    {
+        public FrmMasterTouroku_CustomerSKUPrice()
+        {
+            InitializeComponent();
+        }
+    }
+}
