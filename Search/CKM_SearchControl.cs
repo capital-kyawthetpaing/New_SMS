a65a60b7f0042d90ddd471f4395968a50b14d894
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2020-10-15 09:34:19 +0630

    by SES

--- Search/CKM_SearchControl.cs
+++ Search/CKM_SearchControl.cs
@@ -694,13 +694,13 @@ namespace Search
 
                 case SearchType.商品分類://SES
                     TxtCode.MaxLength = 5;
-                    TxtCode.Width = 100;
+                    TxtCode.Width = 60;
                     lblName.Width = 280;
                     break;
 
                 case SearchType.展示会名://ses 9/7/2020
                     TxtCode.MaxLength = 40;
-                    TxtCode.Width = 400;
+                    TxtCode.Width = 500;
                     lblName.Width = 180;
                     break;
 
