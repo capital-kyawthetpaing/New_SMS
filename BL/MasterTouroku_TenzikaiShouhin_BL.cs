0b2d41cf457faf2ff41a50f4b3871de3023718df
capital-poeeisan <capital.poeeisan@gmail.com>
2020-10-20 13:35:01 +0630

    pes

--- BL/MasterTouroku_TenzikaiShouhin_BL.cs
+++ BL/MasterTouroku_TenzikaiShouhin_BL.cs
@@ -14,14 +14,15 @@ namespace BL
         MasterTouroku_TenzikaiShouhin_DL dl = new MasterTouroku_TenzikaiShouhin_DL();
        public DataTable Mastertoroku_Tenzikaishouhin_Select(M_TenzikaiShouhin_Entity mt)
        {
-
             return dl.Mastertoroku_Tenzikaishouhin_Select(mt);
        }
-
         public DataTable M_Tenzikaishouhin_SelectForJancd(M_TenzikaiShouhin_Entity mt)
         {
-
             return dl.M_Tenzikaishouhin_SelectForJancd(mt);
+        }
+        public DataTable M_SKU_SelectForSKUCheck(M_SKU_Entity msku)
+        {
+            return dl.M_SKU_SelectForSKUCheck(msku);
         }
     }
 }
