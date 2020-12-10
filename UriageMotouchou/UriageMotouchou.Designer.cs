937a250f008787df152993c457f08b26f36a9274
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2020-12-10 14:19:19 +0630

    by ses

--- UriageMotouchou/UriageMotouchou.Designer.cs
+++ UriageMotouchou/UriageMotouchou.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Panel_Details = new System.Windows.Forms.Panel();
             this.chkNo = new CKM_Controls.CKM_CheckBox();
             this.chkYes = new CKM_Controls.CKM_CheckBox();
             this.ckM_Label5 = new CKM_Controls.CKM_Label();
@@ -38,15 +37,13 @@
             this.sc_Customer = new Search.CKM_SearchControl();
             this.txtTagetFrom = new CKM_Controls.CKM_TextBox();
             this.ckM_Label1 = new CKM_Controls.CKM_Label();
-            this.PanelHeader.SuspendLayout();
+            this.Panel_Details = new System.Windows.Forms.Panel();
             this.Panel_Details.SuspendLayout();
             this.SuspendLayout();
             // 
             // PanelHeader
             // 
-            this.PanelHeader.Controls.Add(this.Panel_Details);
-            this.PanelHeader.Size = new System.Drawing.Size(1711, 194);
-            this.PanelHeader.Controls.SetChildIndex(this.Panel_Details, 0);
+            this.PanelHeader.Size = new System.Drawing.Size(1711, 0);
             // 
             // PanelSearch
             // 
@@ -56,27 +53,11 @@
             // 
             this.btnChangeIkkatuHacchuuMode.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             // 
-            // Panel_Details
-            // 
-            this.Panel_Details.Controls.Add(this.chkNo);
-            this.Panel_Details.Controls.Add(this.chkYes);
-            this.Panel_Details.Controls.Add(this.ckM_Label5);
-            this.Panel_Details.Controls.Add(this.cboStore);
-            this.Panel_Details.Controls.Add(this.ckM_Label4);
-            this.Panel_Details.Controls.Add(this.ckM_Label3);
-            this.Panel_Details.Controls.Add(this.sc_Customer);
-            this.Panel_Details.Controls.Add(this.txtTagetFrom);
-            this.Panel_Details.Controls.Add(this.ckM_Label1);
-            this.Panel_Details.Location = new System.Drawing.Point(11, 7);
-            this.Panel_Details.Name = "Panel_Details";
-            this.Panel_Details.Size = new System.Drawing.Size(800, 130);
-            this.Panel_Details.TabIndex = 14;
-            // 
             // chkNo
             // 
             this.chkNo.AutoSize = true;
             this.chkNo.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Bold);
-            this.chkNo.Location = new System.Drawing.Point(185, 94);
+            this.chkNo.Location = new System.Drawing.Point(191, 112);
             this.chkNo.Name = "chkNo";
             this.chkNo.Size = new System.Drawing.Size(37, 16);
             this.chkNo.TabIndex = 32;
@@ -88,7 +69,7 @@
             this.chkYes.AutoSize = true;
             this.chkYes.Cursor = System.Windows.Forms.Cursors.Default;
             this.chkYes.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Bold);
-            this.chkYes.Location = new System.Drawing.Point(113, 94);
+            this.chkYes.Location = new System.Drawing.Point(124, 112);
             this.chkYes.Name = "chkYes";
             this.chkYes.Size = new System.Drawing.Size(37, 16);
             this.chkYes.TabIndex = 31;
@@ -104,7 +85,7 @@
             this.ckM_Label5.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Bold);
             this.ckM_Label5.Font_Size = CKM_Controls.CKM_Label.CKM_FontSize.Normal;
             this.ckM_Label5.ForeColor = System.Drawing.Color.Black;
-            this.ckM_Label5.Location = new System.Drawing.Point(60, 95);
+            this.ckM_Label5.Location = new System.Drawing.Point(77, 113);
             this.ckM_Label5.Name = "ckM_Label5";
             this.ckM_Label5.Size = new System.Drawing.Size(44, 12);
             this.ckM_Label5.TabIndex = 30;
@@ -121,7 +102,7 @@
             this.cboStore.Flag = 0;
             this.cboStore.FormattingEnabled = true;
             this.cboStore.Length = 10;
-            this.cboStore.Location = new System.Drawing.Point(107, 61);
+            this.cboStore.Location = new System.Drawing.Point(124, 80);
             this.cboStore.MaxLength = 10;
             this.cboStore.MoveNext = true;
             this.cboStore.Name = "cboStore";
@@ -138,7 +119,7 @@
             this.ckM_Label4.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Bold);
             this.ckM_Label4.Font_Size = CKM_Controls.CKM_Label.CKM_FontSize.Normal;
             this.ckM_Label4.ForeColor = System.Drawing.Color.Black;
-            this.ckM_Label4.Location = new System.Drawing.Point(34, 65);
+            this.ckM_Label4.Location = new System.Drawing.Point(51, 83);
             this.ckM_Label4.Name = "ckM_Label4";
             this.ckM_Label4.Size = new System.Drawing.Size(70, 12);
             this.ckM_Label4.TabIndex = 28;
@@ -155,7 +136,7 @@
             this.ckM_Label3.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Bold);
             this.ckM_Label3.Font_Size = CKM_Controls.CKM_Label.CKM_FontSize.Normal;
             this.ckM_Label3.ForeColor = System.Drawing.Color.Black;
-            this.ckM_Label3.Location = new System.Drawing.Point(73, 35);
+            this.ckM_Label3.Location = new System.Drawing.Point(90, 49);
             this.ckM_Label3.Name = "ckM_Label3";
             this.ckM_Label3.Size = new System.Drawing.Size(31, 12);
             this.ckM_Label3.TabIndex = 27;
@@ -178,7 +159,7 @@
             this.sc_Customer.IsCopy = false;
             this.sc_Customer.LabelText = "";
             this.sc_Customer.LabelVisible = true;
-            this.sc_Customer.Location = new System.Drawing.Point(107, 27);
+            this.sc_Customer.Location = new System.Drawing.Point(124, 41);
             this.sc_Customer.Margin = new System.Windows.Forms.Padding(0);
             this.sc_Customer.Name = "sc_Customer";
             this.sc_Customer.NameWidth = 500;
@@ -186,6 +167,7 @@
             this.sc_Customer.Size = new System.Drawing.Size(634, 27);
             this.sc_Customer.Stype = Search.CKM_SearchControl.SearchType.得意先;
             this.sc_Customer.TabIndex = 26;
+            this.sc_Customer.test = null;
             this.sc_Customer.TextSize = Search.CKM_SearchControl.FontSize.Normal;
             this.sc_Customer.UseChangeDate = false;
             this.sc_Customer.Value1 = null;
@@ -214,7 +196,7 @@
             this.txtTagetFrom.IsNumber = true;
             this.txtTagetFrom.IsShop = false;
             this.txtTagetFrom.Length = 6;
-            this.txtTagetFrom.Location = new System.Drawing.Point(107, 6);
+            this.txtTagetFrom.Location = new System.Drawing.Point(124, 13);
             this.txtTagetFrom.MaxLength = 6;
             this.txtTagetFrom.MoveNext = true;
             this.txtTagetFrom.Name = "txtTagetFrom";
@@ -233,7 +215,7 @@
             this.ckM_Label1.Font = new System.Drawing.Font("MS Gothic", 9F, System.Drawing.FontStyle.Bold);
             this.ckM_Label1.Font_Size = CKM_Controls.CKM_Label.CKM_FontSize.Normal;
             this.ckM_Label1.ForeColor = System.Drawing.Color.Black;
-            this.ckM_Label1.Location = new System.Drawing.Point(47, 9);
+            this.ckM_Label1.Location = new System.Drawing.Point(64, 16);
             this.ckM_Label1.Name = "ckM_Label1";
             this.ckM_Label1.Size = new System.Drawing.Size(57, 12);
             this.ckM_Label1.TabIndex = 22;
@@ -241,19 +223,36 @@
             this.ckM_Label1.Text_Color = CKM_Controls.CKM_Label.CKM_Color.Default;
             this.ckM_Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
+            // Panel_Details
+            // 
+            this.Panel_Details.Controls.Add(this.ckM_Label1);
+            this.Panel_Details.Controls.Add(this.chkNo);
+            this.Panel_Details.Controls.Add(this.cboStore);
+            this.Panel_Details.Controls.Add(this.sc_Customer);
+            this.Panel_Details.Controls.Add(this.chkYes);
+            this.Panel_Details.Controls.Add(this.txtTagetFrom);
+            this.Panel_Details.Controls.Add(this.ckM_Label4);
+            this.Panel_Details.Controls.Add(this.ckM_Label5);
+            this.Panel_Details.Controls.Add(this.ckM_Label3);
+            this.Panel_Details.Location = new System.Drawing.Point(2, 41);
+            this.Panel_Details.Name = "Panel_Details";
+            this.Panel_Details.Size = new System.Drawing.Size(1710, 140);
+            this.Panel_Details.TabIndex = 100;
+            // 
             // UriageMotouchou
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1713, 961);
+            this.Controls.Add(this.Panel_Details);
             this.Location = new System.Drawing.Point(0, 0);
             this.ModeVisible = true;
             this.Name = "UriageMotouchou";
-            this.PanelHeaderHeight = 250;
+            this.PanelHeaderHeight = 40;
             this.Text = "UriageMotouchou";
             this.Load += new System.EventHandler(this.UriageMotouchou_Load);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.UriageMotouchou_KeyUp);
-            this.PanelHeader.ResumeLayout(false);
+            this.Controls.SetChildIndex(this.Panel_Details, 0);
             this.Panel_Details.ResumeLayout(false);
             this.Panel_Details.PerformLayout();
             this.ResumeLayout(false);
@@ -262,8 +261,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel Panel_Details;
         private CKM_Controls.CKM_CheckBox chkNo;
         private CKM_Controls.CKM_CheckBox chkYes;
         private CKM_Controls.CKM_Label ckM_Label5;
@@ -273,6 +270,7 @@
         private Search.CKM_SearchControl sc_Customer;
         private CKM_Controls.CKM_TextBox txtTagetFrom;
         private CKM_Controls.CKM_Label ckM_Label1;
+        private System.Windows.Forms.Panel Panel_Details;
     }
 }
 
