8a4498dae4eb30da8e5bf1c0bbaccaf81560ec1f
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2020-12-11 08:36:49 +0630

    pnz

--- BL/FBDataSakusei_BL.cs
+++ BL/FBDataSakusei_BL.cs
@@ -5,12 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Entity;
 using DL;
+using System.Data;
 
 namespace BL
 {
     public class FBDataSakusei_BL:Base_BL
     {
         M_Calendar_DL mcdl = new M_Calendar_DL();
+        M_Calendar_Entity mce = new M_Calendar_Entity();
+
+        public DataTable M_Calendar_SelectForFB(M_Calendar_Entity mce)
+        {
+           return mcdl.M_Calendar_SelectForFB(mce);
+        }
 
     }
 }
