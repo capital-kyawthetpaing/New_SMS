a3c2d04bf2901c373b24eb4726c4ed72d69fd80f
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2020-12-11 17:33:55 +0630

    pnz

--- BL/FBDataSakusei_BL.cs
+++ BL/FBDataSakusei_BL.cs
@@ -13,10 +13,17 @@ namespace BL
     {
         M_Calendar_DL mcdl = new M_Calendar_DL();
         M_Calendar_Entity mce = new M_Calendar_Entity();
+        D_Pay_DL dpdl = new D_Pay_DL();
+        D_Pay_Entity dpe = new D_Pay_Entity();
 
         public DataTable M_Calendar_SelectForFB(M_Calendar_Entity mce)
         {
            return mcdl.M_Calendar_SelectForFB(mce);
+        }
+
+        public DataTable D_Pay_SelectForFB(D_Pay_Entity dpe)
+        {
+            return dpdl.D_Pay_SelectForFB(dpe);
         }
 
     }
