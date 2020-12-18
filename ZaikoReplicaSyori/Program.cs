8320227e9322d7c6951853098b190d9aacadc3cd
Saw Yupar Kyaw <capital-sawyuparkyaw>
2020-12-18 13:03:32 +0630

    By sawlay

--- ZaikoReplicaSyori/Program.cs
+++ ZaikoReplicaSyori/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BL;
+using Entity;
+using System.Data;
 
 namespace ZaikoReplicaSyori
 {
     class Program
     {
+        static Login_BL loginbl = new Login_BL();
+        static M_MultiPorpose_Entity mmpe = new M_MultiPorpose_Entity();
+
         static void Main(string[] args)
         {
+            Console.Title = "ZaikoReplicaSyori";
+            if (loginbl.ReadConfig() == true)
+            {
+                mmpe.ID = "255";
+                mmpe.Key = "1";
+
+            }
         }
     }
 }
