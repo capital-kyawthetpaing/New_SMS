ae9df14b172cba60003a3da7be6d3f20d400f50e
capital-eithinzarzaw <60596990+capital-eithinzarzaw@users.noreply.github.com>
2020-12-18 11:29:50 +0630

    etz

---
+++ MailRecieve/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL;
+using Entity;
+using System.Data;
+
+namespace MailRecieve
+{
+    class Program
+    {
+        static Login_BL loginbl = new Login_BL();
+        static M_MultiPorpose_Entity mmpe = new M_MultiPorpose_Entity();
+        static DataTable dtMulti;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "FileTransferProtocolInput";
+
+            if (loginbl.ReadConfig() == true)
+            {
+                mmpe.ID = "324";
+                mmpe.Key = "1";
+               // dtMulti = ftpsbl.M_MultiPorpose_SelectID(mmpe);
+
+                //【起動可否確認】
+                //汎用マスター.	数字型１＝０なら、処理終了
+                //if (dtMulti.Rows[0]["Num1"].ToString().Equals("0"))
+                //    return;
+
+                //ftpi.FTPImport();
+            }
+        }
+    }
+}
