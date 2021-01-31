a359c26061750c3b7b1d59f45253075c61fe259e
watanabe <watanabe@dus.co.jp>
2021-01-31 17:43:55 +0900

    watanabe 出荷入力変更

---
+++ BL/CarrierDeliveryTime_BL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using DL;
+using System.Data;
+
+namespace BL
+{
+    public class CarrierDeliveryTime_BL : Base_BL
+    {
+        M_CarrierDeliveryTime_DL mdl;
+        public CarrierDeliveryTime_BL()
+        {
+            mdl = new M_CarrierDeliveryTime_DL();
+        }
+
+        public DataTable M_CarrierDeliveryTime_Bind(M_CarrierDeliveryTime_Entity mce)
+        {
+            return mdl.M_CarrierDeliveryTime_Bind(mce);
+        }
+    }
+}
---
+++ Entity/M_CarrierDeliveryTime_Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class M_CarrierDeliveryTime_Entity : Base_Entity
+    {
+        public string CarrierCD {get;set;}
+        public string DeliveryTimeCD { get; set; }
+        public string DeliveryTime { get; set; }
+    }
+}
