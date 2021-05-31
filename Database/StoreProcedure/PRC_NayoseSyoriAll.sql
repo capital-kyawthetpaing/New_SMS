7520ec6cb00bce45cc97b9febbc1e6f32c18917a
Fumi Sato <hesaka@dus.co.jp>
2021-05-31 20:00:11 +0900

    hesaka 名寄せ処理　追加

--- Database/StoreProcedure/PRC_NayoseSyoriAll.sql
+++ Database/StoreProcedure/PRC_NayoseSyoriAll.sql
@@ -29,6 +29,7 @@ BEGIN
     SET @SYSDATETIME = SYSDATETIME();
     SET @OperateModeNm = '名寄せ処理';
     
+    --名寄せ対象の受注データを抽出し、プログラム内受注ワークへ更新。
     DECLARE CUR_Juchu CURSOR FOR
         SELECT DH.JuchuuNO
               ,DH.CustomerName
@@ -47,7 +48,11 @@ BEGIN
             AND DH.CustomerCD IS NULL
             AND DH.DeleteDateTime IS NULL
             ;
-
+            
+	DECLARE @CNT1 int;
+	DECLARE @CNT2 int;
+	DECLARE @CNT3 int;
+	DECLARE @CNT4 int;
     DECLARE @JuchuuNO varchar(11);
     DECLARE @CustomerCD varchar(13);
     DECLARE @CustomerName   varchar(80);
@@ -66,7 +71,7 @@ BEGIN
 
     --最初の1行目を取得して変数へ値をセット
     FETCH NEXT FROM CUR_Juchu
-    INTO @JuchuuNO,@CustomerName,@CustomerName2,@Tel11,Tel12,Tel13,@MailAddress,@Address1,@Address2 ;
+    INTO @JuchuuNO,@CustomerName,@CustomerName2,@Tel11,@Tel12,@Tel13,@MailAddress,@Address1,@Address2 ;
     
     --データの行数分ループ処理を実行する
     WHILE @@FETCH_STATUS = 0
@@ -74,11 +79,190 @@ BEGIN
     -- ========= ループ内の実際の処理 ここから===*************************CUR_Stock
         --次の行のデータを取得して変数へ値をセット
         FETCH NEXT FROM CUR_Juchu
-        INTO @JuchuuNO,@CustomerName,@CustomerName2,@Tel11,Tel12,Tel13,@MailAddress,@Address1,@Address2 ;
-        
+        INTO @JuchuuNO,@CustomerName,@CustomerName2,@Tel11,@Tel12,@Tel13,@MailAddress,@Address1,@Address2 ;
+        --受注ワークを1件リード
+
         SET @W_ERR = 0;
         
+        --名寄せ用の顧客マスタを抽出し、プログラム内顧客ワークへ更新
         
+        --顧客ワークと名寄せチェック
+        --①名前 ＆ 電話番号 ＆ メールアドレス一致
+        SET @CNT1= (SELECT COUNT(FC.CustomerCD)
+                    from D_Juchuu AS DH
+                   INNER JOIN F_Customer(GETDATE()) FC
+                      ON FC.CustomerName = DH.CustomerName
+                     AND ISNULL(FC.Tel11,'') + '-' + ISNULL(FC.Tel12,'') + '-' + ISNULL(FC.Tel13,'') = ISNULL(DH.Tel11,'') + '-' + ISNULL(DH.Tel12,'') + '-' + ISNULL(DH.Tel13,'')
+                     AND dbo.Fnc_MailAdress(FC.MailAddress) = dbo.Fnc_MailAdress(DH.MailAddress)                                      
+                   WHERE DH.JuchuuNO = @JuchuuNO
+                     AND DH.DeleteDateTime IS NULL
+        			)
+        IF @CNT1 = 1
+        BEGIN
+            SET @CustomerCD = (SELECT FC.CustomerCD
+                                 from D_Juchuu AS DH
+                                INNER JOIN F_Customer(GETDATE()) FC
+                                   ON FC.CustomerName = DH.CustomerName
+                                  AND ISNULL(FC.Tel11,'') + '-' + ISNULL(FC.Tel12,'') + '-' + ISNULL(FC.Tel13,'') = ISNULL(DH.Tel11,'') + '-' + ISNULL(DH.Tel12,'') + '-' + ISNULL(DH.Tel13,'')
+                                  AND dbo.Fnc_MailAdress(FC.MailAddress) = dbo.Fnc_MailAdress(DH.MailAddress)                                                  
+                                WHERE DH.JuchuuNO = @JuchuuNO
+                                  AND DH.DeleteDateTime IS NULL
+                                );
+            
+            --上記Select件数が1件の時、D_Juchuu.顧客CDにM_Customer.顧客CDをUpdate。
+            --D_Juchuu.名寄せ対象FLGに0をUpdate。
+            UPDATE D_Juchuu SET
+                 [CustomerCD]        = @CustomerCD
+                ,[IdentificationFLG] = 0
+                ,[UpdateOperator]    = @Operator  
+                ,[UpdateDateTime]    = @SYSDATETIME
+            WHERE JuchuuNO = @JuchuuNO
+            ;
+       		
+       		--テーブル転送仕様Ｂに従って受注保留警告(D_JuchuuOnHold)のレコード変更。(※1）
+			
+			--テーブル転送仕様Ｃに従って受注ステータス(D_JuchuuStatus)のレコード追加もしくは変更。
+			
+			--次の「1.受注ワークを1件リード」へ。
+		END
+		ELSE IF @CNT1 = 0
+		BEGIN
+			--上記Select件数が0件の時、②の名寄せチェックへ
+			
+			--②名前 ＆ 電話番号
+        	SET @CNT2= (SELECT FC.CustomerCD
+	                      from D_Juchuu AS DH
+	                     INNER JOIN F_Customer(GETDATE()) FC
+	                        ON FC.CustomerName = DH.CustomerName
+	                       AND ISNULL(FC.Tel11,'') + '-' + ISNULL(FC.Tel12,'') + '-' + ISNULL(FC.Tel13,'') = ISNULL(DH.Tel11,'') + '-' + ISNULL(DH.Tel12,'') + '-' + ISNULL(DH.Tel13,'')
+	                       AND FC.StoreKBN = 1
+	                     WHERE DH.JuchuuNO = @JuchuuNO
+	                       AND DH.DeleteDateTime IS NULL
+	        			)
+	        IF @CNT2 >= 1
+	        BEGIN
+	        	--上記Select件数が1件の時、D_Juchuu.保留FLG、名寄せ対象FLGに1をUpdate。
+	            UPDATE D_Juchuu SET
+	                 [OnHoldFLG]         = 1
+	                ,[IdentificationFLG] = 1
+	                ,[UpdateOperator]    = @Operator  
+	                ,[UpdateDateTime]    = @SYSDATETIME
+	            WHERE JuchuuNO = @JuchuuNO
+	            ;
+	            
+	            --テーブル転送仕様Ｂに従って受注保留警告(D_JuchuuOnHold)のレコード追加。
+	            
+	            --テーブル転送仕様Ｃに従って受注ステータス(D_JuchuuStatus)のレコード変更。
+	            
+	            --次の「1.受注ワークを1件リード」へ。
+	        END
+	        ELSE IF @CNT2 = 0
+	        BEGIN
+	        	--上記Select件数が0件の時、③の名寄せチェックへ
+	        	
+				--③名前 ＆ メールアドレス
+		        SET @CNT3 = (SELECT FC.CustomerCD
+		                       from D_Juchuu AS DH
+		                      INNER JOIN F_Customer(GETDATE()) FC
+		                         ON FC.CustomerName = DH.CustomerName
+		                        AND dbo.Fnc_MailAdress(FC.MailAddress) = dbo.Fnc_MailAdress(DH.MailAddress) 
+		                        AND FC.StoreKBN = 1
+		                      WHERE DH.JuchuuNO = @JuchuuNO
+		                        AND DH.DeleteDateTime IS NULL
+		        			)
+		        IF @CNT3 >= 1
+		        BEGIN
+		        	--上記Select件数が1件の時、
+		        	--D_Juchuu.保留FLG、名寄せ対象FLGに1をUpdate。
+		            UPDATE D_Juchuu SET
+		                 [OnHoldFLG]         = 1
+		                ,[IdentificationFLG] = 1
+		                ,[UpdateOperator]    = @Operator  
+		                ,[UpdateDateTime]    = @SYSDATETIME
+		            WHERE JuchuuNO = @JuchuuNO
+		            ;
+		            
+		            --テーブル転送仕様Ｂに従って受注保留警告(D_JuchuuOnHold)のレコード追加。
+		            
+		            --テーブル転送仕様Ｃに従って受注ステータス(D_JuchuuStatus)のレコード変更。
+		            
+	            	--次の「1.受注ワークを1件リード」へ。
+
+		        END
+		        ELSE IF @CNT3 = 0
+		        BEGIN
+		        	--④の名寄せチェックへ
+		        	--④名前 ＆ 住所１	
+		        	SET @CNT4= (SELECT FC.CustomerCD
+			                      from D_Juchuu AS DH
+			                     INNER JOIN F_Customer(GETDATE()) FC
+			                        ON FC.CustomerName = DH.CustomerName
+			                       AND dbo.Fnc_AdressHalfToFull(FC.Address1) = dbo.Fnc_AdressHalfToFull(DH.Address1)
+			                       AND FC.StoreKBN = 1
+			                     WHERE DH.JuchuuNO = @JuchuuNO
+			                       AND DH.DeleteDateTime IS NULL
+			        			)
+		        	IF @CNT4 >= 1
+		        	BEGIN
+		        		--上記Select件数が1件の時、
+			        	--D_Juchuu.保留FLG、名寄せ対象FLGに1をUpdate。
+			            UPDATE D_Juchuu SET
+			                 [OnHoldFLG]         = 1
+			                ,[IdentificationFLG] = 1
+			                ,[UpdateOperator]    = @Operator  
+			                ,[UpdateDateTime]    = @SYSDATETIME
+			            WHERE JuchuuNO = @JuchuuNO
+			            ;
+		            
+                        --テーブル転送仕様Ｂに従って受注保留警告(D_JuchuuOnHold)のレコード追加。
+                        
+                        --テーブル転送仕様Ｃに従って受注ステータス(D_JuchuuStatus)のレコード変更。
+                        
+                        --次の「1.受注ワークを1件リード」へ。
+
+		        	END
+		        	ELSE IF @CNT4 = 0
+		        	BEGIN
+		        		--上記Select件数が0件の時、
+		        		--⑤の顧客マスタ登録へ
+		        		--テーブル転送仕様Ａに従って顧客マスタのレコード追加。
+		        		
+		        		
+		        		
+		        		--採番した顧客CDをD_Juchuu.顧客CDにUpdate。
+			            UPDATE D_Juchuu SET
+			                 [CustomerCD]        = @CustomerCD
+			                ,[UpdateOperator]    = @Operator  
+			                ,[UpdateDateTime]    = @SYSDATETIME
+			            WHERE JuchuuNO = @JuchuuNO
+			            ;
+
+		        		
+
+
+		        	END
+		        END		        
+	        END
+	    END
+		ELSE
+		BEGIN
+			--上記Select件数が複数件の時、
+			--D_Juchuu.保留FLG、名寄せ対象FLGに1をUpdate。
+            UPDATE D_Juchuu SET
+                 [OnHoldFLG]         = 1
+                ,[IdentificationFLG] = 1
+                ,[UpdateOperator]    = @Operator  
+                ,[UpdateDateTime]    = @SYSDATETIME
+            WHERE JuchuuNO = @JuchuuNO
+            ;
+            
+            --テーブル転送仕様Ｂに従って受注保留警告(D_JuchuuOnHold)のレコード追加。
+            
+            --テーブル転送仕様Ｃに従って受注ステータス(D_JuchuuStatus)のレコード追加もしくは変更。
+            
+            --次の「1.受注ワークを1件リード」へ。
+
+		END
         
     END     --LOOPの終わり***************************************CUR_Stock
     
@@ -126,7 +310,7 @@ BEGIN
         @OperateModeNm,
         @KeyItem;
 
-    SET @OutJuchuuNo = '';
+    SET @OutErrNo = '';
     
 --<<OWARI>>
   return @W_ERR;
--- NayoseSyoriAll/NayoseSyoriAll.cs
+++ NayoseSyoriAll/NayoseSyoriAll.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 
 using BL;
 using Entity;
 using Base.Client;
-using Search;
-using GridBase;
 
 namespace NayoseSyoriAll
 {
@@ -22,18 +15,14 @@ namespace NayoseSyoriAll
         private const string ProID = "NayoseSyoriAll";
         private const string ProNm = "名寄せ処理(全顧客)";
         private const short mc_L_END = 3; // ロック用
-        
+
         private NayoseSyoriAll_BL nkbl;
         private D_Juchuu_Entity dje;
-
-        private DataTable dtForUpdate;      //排他用   
-        private string mOldJuchuNO = "";    //排他処理のため使用
-        private string mOldPickingDate = "";
 
         public NayoseSyoriAll()
         {
             InitializeComponent();
-            
+
         }
 
         private void Form_Load(object sender, EventArgs e)
@@ -64,7 +53,7 @@ namespace NayoseSyoriAll
                 Btn_F12.Focus();
 
                 nkbl = new NayoseSyoriAll_BL();
-                            }
+            }
             catch (Exception ex)
             {
                 //エラー時共通処理
@@ -74,142 +63,6 @@ namespace NayoseSyoriAll
             }
         }
 
-        private bool SelectAndInsertExclusive(Exclusive_BL.DataKbn kbn, string No)
-        {
-            if (OperationMode == EOperationMode.SHOW)
-                return true;
-
-            //排他Tableに該当番号が存在するとError
-            //[D_Exclusive]
-            Exclusive_BL ebl = new Exclusive_BL();
-            D_Exclusive_Entity dee = new D_Exclusive_Entity
-            {
-                DataKBN = (int)kbn,
-                Number = No,
-                Program = this.InProgramID,
-                Operator = this.InOperatorCD,
-                PC = this.InPcID
-            };
-
-            DataTable dt = ebl.D_Exclusive_Select(dee);
-
-            if (dt.Rows.Count > 0)
-            {
-                bbl.ShowMessage("S004", dt.Rows[0]["Program"].ToString(), dt.Rows[0]["Operator"].ToString());
-                PreviousCtrl.Focus();
-                return false;
-            }
-            else
-            {
-                bool ret = ebl.D_Exclusive_Insert(dee);
-                return ret;
-            }
-        }
-        /// <summary>
-        /// 排他処理データを削除する
-        /// </summary>
-        private void DeleteExclusive()
-        {
-            if (dtForUpdate == null)
-                return;
-
-            Exclusive_BL ebl = new Exclusive_BL();
-
-            if (dtForUpdate != null)
-            {
-                mOldJuchuNO = "";
-                foreach (DataRow dr in dtForUpdate.Rows)
-                {
-                    D_Exclusive_Entity de = new D_Exclusive_Entity
-                    {
-                        DataKBN = Convert.ToInt16(dr["kbn"]),
-                        Number = dr["no"].ToString()
-                    };
-
-                    ebl.D_Exclusive_Delete(de);
-                }
-                return;
-            }
-        }
-        /// <summary>
-        /// データ取得処理
-        /// </summary>
-        /// <returns></returns>
-        private bool CheckData()
-        {
-            M_Customer_Entity mce = new M_Customer_Entity();
-            mce.StoreKBN = "1";//1（WEB)
-            DataTable dtCustomer = nkbl.M_Customer_SelectForNayose(mce);
-
-            //[D_Juchu_SelectForNayose]
-            //dje = GetSearchInfo();
-
-            DataTable dt = nkbl.D_Juchu_SelectForNayose(dje);
-
-            //ヘッダ部.名寄せ実施日＝空白　の時
-            //画面転送表01に従って、画面情報を表示
-
-            //ヘッダ部.名寄せ実施日<>空白　の時
-            //画面転送表02に従って、画面情報を表示
-
-            //テーブル転送仕様Ｙに従って、排他テーブルにレコード追加
-            DeleteExclusive();
-
-            dtForUpdate = new DataTable();
-            dtForUpdate.Columns.Add("kbn", Type.GetType("System.String"));
-            dtForUpdate.Columns.Add("no", Type.GetType("System.String"));
-
-            bool ret;
-            //排他処理
-            foreach (DataRow row in dt.Rows)
-            {                
-                if (mOldJuchuNO != row["JuchuNo"].ToString() && !string.IsNullOrWhiteSpace(row["JuchuNo"].ToString()))
-                {
-                    ret = SelectAndInsertExclusive(Exclusive_BL.DataKbn.Jyuchu, row["JuchuNo"].ToString());
-                    if (!ret)
-                        return false;
-
-                    mOldJuchuNO = row["JuchuNo"].ToString();
-
-                    // データを追加
-                    DataRow rowForUpdate;
-                    rowForUpdate = dtForUpdate.NewRow();
-                    rowForUpdate["kbn"] = (int)Exclusive_BL.DataKbn.Jyuchu;
-                    rowForUpdate["no"] = mOldJuchuNO;
-                    dtForUpdate.Rows.Add(rowForUpdate);
-                }
-            }
-
-
-            //ピッキング(D_Juchuu)に存在しない場合、Error 「登録されていないピッキング番号」
-            if (dt.Rows.Count == 0)
-            {
-                bbl.ShowMessage("E138", "ピッキング番号");
-                PreviousCtrl.Focus();
-                return false;
-            }
-            else
-            {
-                //DeleteDateTime 「削除されたピッキング番号」
-                if (!string.IsNullOrWhiteSpace(dt.Rows[0]["DeleteDateTime"].ToString()))
-                {
-                    bbl.ShowMessage("E140", "ピッキング番号");
-                    PreviousCtrl.Focus();
-                    return false;
-                }
-
-                ////権限がない場合（以下のSelectができない場合）Error　「権限のないピッキング番号」
-                //if (!base.CheckAvailableStores(dt.Rows[0]["StoreCD"].ToString()))
-                //{
-                //    bbl.ShowMessage("E139", "ピッキング番号");
-                //    Scr_Clr(1);
-                //    previousCtrl.Focus();
-                //    return false;
-                //}
-            }
-            return true;
-        }
-       
         /// <summary>
         /// 画面情報をセット
         /// </summary>
@@ -227,43 +80,25 @@ namespace NayoseSyoriAll
 
         protected override void ExecSec()
         {
-      
+            //Ｑ００２		
+            if (bbl.ShowMessage("Q002") != DialogResult.Yes)
+                return;
+
             //更新処理
             dje = GetEntity();
             nkbl.NayoseSyoriAll_Exec(dje);
 
-            //ログファイルへの更新
-            bbl.L_Log_Insert(Get_L_Log_Entity());
-
-                bbl.ShowMessage("I101");
-           
-        }
-        /// <summary>
-        /// get Log information
-        /// print log
-        /// </summary>
-        private L_Log_Entity Get_L_Log_Entity()
-        {
-            ////画面指定項目をカンマ編集で羅列（ex."2019/07/01,2019/7/31,ABCDEFG,未出力"）
-            //string item = keyControls[0].Text;
-            //for (int i = 1; i <= (int)EIndex.JanCD; i++)
-            //{
-            //    item += "," + keyControls[i].Text;
-            //}
-
-            L_Log_Entity lle = new L_Log_Entity
+            if (!string.IsNullOrWhiteSpace(dje.ReturnFLG))
             {
-                InsertOperator = this.InOperatorCD,
-                PC = this.InPcID,
-                Program = this.InProgramID,
-                OperateMode = "寄せ結果登録",
-                KeyItem = ""
-            };
+                bbl.ShowMessage(dje.ReturnFLG);
+                return;
+            }
 
-            return lle;
+            bbl.ShowMessage("I002");
+
         }
 
-             /// <summary>
+        /// <summary>
         /// handle f1 to f12 click event
         /// implement base virtual function
         /// </summary>
@@ -285,15 +120,15 @@ namespace NayoseSyoriAll
                 case 5: //F6:キャンセル
                     break;
 
-                case 11:    //F12:登録
-                    {
-                        //Ｑ１０１		
-                        if (bbl.ShowMessage("Q101") != DialogResult.Yes)
-                            return;
+                    //case 11:    //F12:登録
+                    //    {
+                    //        //Ｑ１０１		
+                    //        if (bbl.ShowMessage("Q101") != DialogResult.Yes)
+                    //            return;
 
-                        this.ExecSec();
-                        break;
-                    }
+                    //        this.ExecSec();
+                    //        break;
+                    //    }
             }   //switch end
 
         }
@@ -309,9 +144,8 @@ namespace NayoseSyoriAll
             //Application.Exit();
             //System.Environment.Exit(0);
         }
-                   }
+    }
 }
-
 
 
 
