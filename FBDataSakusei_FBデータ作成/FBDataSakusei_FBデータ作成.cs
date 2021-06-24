2061e255bcccebef54816e750965cba54e9c2b6d
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2021-06-24 10:44:29 +0630

    ptk

--- FBDataSakusei_FBデータ作成/FBDataSakusei_FBデータ作成.cs
+++ FBDataSakusei_FBデータ作成/FBDataSakusei_FBデータ作成.cs
@@ -25,6 +25,7 @@ namespace FBDataSakusei_FBデータ作成
         D_FBData_Entity dfde = new D_FBData_Entity();
         DataTable dtgv = new DataTable();
         public static string dirParameter = AppDomain.CurrentDomain.BaseDirectory + @"\file.txt";
+         
 
         public FrmFBDataSakusei_FBデータ作成()
         {
